37b346a05eb2c34b1c90f5e3045f78d88cf8b82d
Ahad-Dar01 <ahad.qasim20@gmail.com>
2022-11-27 14:32:35 -0800

    s

---
+++ server.js
@@ -0,0 +1,11 @@
+process.EventEmitter = require('events').EventEmitter;
+var zetta = require('zetta');
+var LED = require('zetta-led-mock-driver');
+const port= process.env.PORT || 3000
+zetta()
+  .name('FirstName-LastName')
+  .use(LED)
+  //.link('https://herokutest-production-c5d3.up.railway.app/')
+  .listen(port, function(){
+     console.log('Zetta is running at ${port}');
+});
